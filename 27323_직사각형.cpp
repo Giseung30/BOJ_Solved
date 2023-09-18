65aa84b769d3d413222304067d25f067c8ba0e2b
giseung30 <giseung30@gmail.com>
2023-09-18 12:44:09 +0900

    230918_27323 해결

---
+++ 27323_직사각형.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define endl "\n"
+using namespace std;
+
+int a, b;
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> a >> b;
+	cout << a * b << endl;
+
+	return 0;
+}
